d994c8b369fa02eb82be4d8511df76ceca5b2174
Konnokai <f20001012@gmail.com>
2023-08-26 23:37:58 +0800

    feature: 說謊基金

---
+++ Discord Support Bot/Interaction/LyingFund/LyingFund.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Interactions;
+
+namespace Discord_Support_Bot.Interaction.LyingFund
+{
+    [DontAutoRegister]
+    [Attribute.RequireGuild(910364272473829396)]
+    public class LyingFund : TopLevelModule
+    {
+        [SlashCommand("lying-fund", "說謊基金")]
+        public async Task LyingFundAsync(IUser user)
+        {
+            var fund = await Program.Redis.GetDatabase(0).HashIncrementAsync("support:LyinhFund", user.Id, 500);
+            await Context.Interaction.SendConfirmAsync($"已對 `{user}` 增加 500 說謊基金，現在金額: {fund}");
+        }
+    }
+}
