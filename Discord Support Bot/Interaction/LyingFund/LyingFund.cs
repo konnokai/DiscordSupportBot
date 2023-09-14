d239cf9f5db636265c3f2160da63349b06e72ce6
孤之界 <f20001012@gmail.com>
2023-09-14 15:47:26 +0800

    feature: 新增 說謊基金排行榜 指令

--- Discord Support Bot/Interaction/LyingFund/LyingFund.cs
+++ Discord Support Bot/Interaction/LyingFund/LyingFund.cs
@@ -12,5 +12,14 @@ namespace Discord_Support_Bot.Interaction.LyingFund
             var fund = await Program.Redis.GetDatabase(0).HashIncrementAsync("support:LyinhFund", user.Id, 500);
             await Context.Interaction.SendConfirmAsync($"已對 `{user}` 增加 500 說謊基金，現在金額: {fund}");
         }
+
+        [SlashCommand("lying-fund-leaderboard", "說謊基金排行榜")]
+        public async Task LyingFundLeaderBoardAsync()
+        {
+            var hashEntries = await Program.Redis.GetDatabase(0).HashGetAllAsync("support:LyinhFund");
+            
+            await Context.Interaction.SendConfirmAsync($"說謊基金排行榜\n\n" +
+                $"{string.Join('\n', hashEntries.OrderByDescending((x) => x.Value).Select((x) => $"<@{x.Name}>: {x.Value}"))}");
+        }
     }
 }
