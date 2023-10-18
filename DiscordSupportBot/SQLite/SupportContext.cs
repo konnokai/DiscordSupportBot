5bd4ddaf92f07214f5ae2cc62dde8f2c281a3b6e
孤之界 <f20001012@gmail.com>
2023-10-18 18:06:41 +0800

    feature: GetDbContext 方法

--- DiscordSupportBot/SQLite/SupportContext.cs
+++ DiscordSupportBot/SQLite/SupportContext.cs
@@ -11,5 +11,19 @@ namespace DiscordSupportBot.SQLite
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite($"Data Source={Program.GetDataFilePath("DataBase.db")}");
+
+        public static SupportContext GetDbContext()
+        {
+            var context = new SupportContext();
+            context.Database.SetCommandTimeout(60);
+            var conn = context.Database.GetDbConnection();
+            conn.Open();
+            using (var com = conn.CreateCommand())
+            {
+                com.CommandText = "PRAGMA journal_mode=WAL; PRAGMA synchronous=OFF";
+                com.ExecuteNonQuery();
+            }
+            return context;
+        }
     }
 }
