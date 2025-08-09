f132c15d39bdec23613eb800d51d31e1b9dc546e
孤之界 <4331068+konnokai@users.noreply.github.com>
2025-08-09 23:22:58 +0800

    feat: 將說謊基金給全球可用化

--- DiscordSupportBot/Interaction/LyingFund/LyingFund.cs
+++ DiscordSupportBot/Interaction/LyingFund/LyingFund.cs
@@ -2,23 +2,27 @@
 
 namespace DiscordSupportBot.Interaction.LyingFund
 {
-    [DontAutoRegister]
-    [Attribute.RequireGuild(910364272473829396)]
     public class LyingFund : TopLevelModule
     {
         [SlashCommand("lying-fund", "說謊基金")]
         public async Task LyingFundAsync(IUser user)
         {
-            var fund = await Program.RedisConnection.GetDatabase(0).HashIncrementAsync("support:LyinhFund", user.Id, 500);
+            var fund = await RedisConnection.RedisDb.HashIncrementAsync($"support:LyinhFund:{Context.Guild.Id}", user.Id, 500);
             await Context.Interaction.SendConfirmAsync($"已對 `{user}` 增加 500 說謊基金，現在金額: {fund}");
         }
 
         [SlashCommand("lying-fund-leaderboard", "說謊基金排行榜")]
         public async Task LyingFundLeaderBoardAsync()
         {
-            var hashEntries = await Program.RedisConnection.GetDatabase(0).HashGetAllAsync("support:LyinhFund");
+            var hashEntries = await RedisConnection.RedisDb.HashGetAllAsync($"support:LyinhFund:{Context.Guild.Id}");
 
-            await Context.Interaction.SendConfirmAsync($"說謊基金排行榜\n\n" +
+            if (hashEntries.Length == 0)
+            {
+                await Context.Interaction.SendErrorAsync("目前沒有任何人有說謊基金。");
+                return;
+            }
+
+            await Context.Interaction.SendConfirmAsync($"`{Context.Guild.Name}` 說謊基金排行榜\n\n" +
                 $"{string.Join('\n', hashEntries.OrderByDescending((x) => x.Value).Select((x) => $"<@{x.Name}>: {x.Value}"))}");
         }
     }
