d7e3b4624e0faffe686e1d849964636cd3b339c2
孤之界 <4331068+konnokai@users.noreply.github.com>
2025-09-08 17:12:21 +0800

    feat: 新增基金類型

--- DiscordSupportBot/Interaction/LyingFund/LyingFund.cs
+++ DiscordSupportBot/Interaction/LyingFund/LyingFund.cs
@@ -4,8 +4,18 @@ namespace DiscordSupportBot.Interaction.LyingFund
 {
     public class LyingFund : TopLevelModule
     {
-        [SlashCommand("lying-fund", "說謊基金")]
-        public async Task LyingFundAsync(IUser user)
+        public enum FundType
+        {
+            [ChoiceDisplay("說謊")]
+            Lying,
+            [ChoiceDisplay("暈船")]
+            Dizzy,
+            [ChoiceDisplay("色狗")]
+            HentaiDog
+        }
+
+        [SlashCommand("add-fund", "對某人添加基金")]
+        public async Task AddFundAsync([Summary("基金類型")] FundType fundType, [Summary("目標使用者")] IUser user)
         {
             if (Context.Guild.GetUser(user.Id) == null)
             {
@@ -13,23 +23,34 @@ namespace DiscordSupportBot.Interaction.LyingFund
                 return;
             }
 
-            var fund = await RedisConnection.RedisDb.HashIncrementAsync($"support:LyinhFund:{Context.Guild.Id}", user.Id, 500);
-            await Context.Interaction.SendConfirmAsync($"已對 `{user}` 增加 500 說謊基金，現在金額: {fund}");
+            var fund = await RedisConnection.RedisDb.HashIncrementAsync($"support:{fundType}Fund:{Context.Guild.Id}", user.Id, 500);
+            await Context.Interaction.SendConfirmAsync($"已對 `{user}` 增加 500 {GetFundTypeName(fundType)}基金，現在金額: {fund}");
         }
 
-        [SlashCommand("lying-fund-leaderboard", "說謊基金排行榜")]
-        public async Task LyingFundLeaderBoardAsync()
+        [SlashCommand("fund-leaderboard", "基金排行榜")]
+        public async Task FundLeaderBoardAsync([Summary("基金類型")] FundType fundType)
         {
-            var hashEntries = await RedisConnection.RedisDb.HashGetAllAsync($"support:LyinhFund:{Context.Guild.Id}");
+            var hashEntries = await RedisConnection.RedisDb.HashGetAllAsync($"support:{fundType}Fund:{Context.Guild.Id}");
 
             if (hashEntries.Length == 0)
             {
-                await Context.Interaction.SendErrorAsync("目前沒有任何人有說謊基金。");
+                await Context.Interaction.SendErrorAsync($"目前沒有任何人有{GetFundTypeName(fundType)}基金");
                 return;
             }
 
-            await Context.Interaction.SendConfirmAsync($"`{Context.Guild.Name}` 說謊基金排行榜\n\n" +
+            await Context.Interaction.SendConfirmAsync($"`{Context.Guild.Name}` {GetFundTypeName(fundType)}基金排行榜\n\n" +
                 $"{string.Join('\n', hashEntries.OrderByDescending((x) => x.Value).Select((x) => $"<@{x.Name}>: {x.Value}"))}");
+        }
+
+        private string GetFundTypeName(FundType fundType)
+        {
+            return fundType switch
+            {
+                FundType.Lying => "說謊",
+                FundType.Dizzy => "暈船",
+                FundType.HentaiDog => "色狗",
+                _ => throw new NotImplementedException(),
+            };
         }
     }
 }
