4411c7cbf346c24121cbd3e28e08ac834660d3f4
Konnokai <4331068+konnokai@users.noreply.github.com>
2026-06-20 19:24:15 +0800

    feat: Add-Migration AddStreamingStatus

---
+++ DiscordSupportBot/Migrations/20260620112334_AddStreamingStatus.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordSupportBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStreamingStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableStreamingStatus",
+                table: "GuildConfig",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreamingStatusTemplate",
+                table: "GuildConfig",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableStreamingStatus",
+                table: "GuildConfig");
+
+            migrationBuilder.DropColumn(
+                name: "StreamingStatusTemplate",
+                table: "GuildConfig");
+        }
+    }
+}
